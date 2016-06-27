c89ca0c60487facaf63e809754444ab1acb5a064
Nate Foreman <nateforeman@users.noreply.github.com>
2016-06-26 21:18:41 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+		
+		<head>
+			<title>Hello Cloud Gurus</title>
+		</head>
+
+		<body>
+		<div align="center">
+			<h1>Hello AWS Students's</h1>
+                <h2>Congratulations! You have successfully deployed a PHP web app to EC2</h2>
+                <div align="center">
+                	<img src="https://s3-us-east-1.amazonaws.com/awsstudent/logo.png">
+                </div>
+                <p>Your Web Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo;</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+    </div>
+    </body>
+
+</html>
