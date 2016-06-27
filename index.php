aa41e38881cad50e3aca5384cc0af918b7f9636c
Nate Foreman <nateforeman@users.noreply.github.com>
2016-06-26 21:39:02 -0400

    Update index.php
    Changed the logo url

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 			<h1>Hello AWS Students's</h1>
                 <h2>Congratulations! You have successfully deployed a PHP web app to EC2</h2>
                 <div align="center">
-                	<img src="https://s3-us-east-1.amazonaws.com/awsstudent/logo.png">
+                	<img src="https://s3.amazonaws.com/awsstudent/logo.png">
                 </div>
                 <p>Your Web Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo;</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
